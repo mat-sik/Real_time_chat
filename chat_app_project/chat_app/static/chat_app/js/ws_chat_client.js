c456a6d3d7fb695b02055e7962c994218d90c190
SIIIKOR <siiikor.sikorski@gmail.com>
2022-06-25 14:33:08 +0200

    moved js code to static

---
+++ chat_app_project/chat_app/static/chat_app/js/ws_chat_client.js
@@ -0,0 +1,36 @@
+const roomName = JSON.parse(
+    document.getElementById("room-name").textContent
+);
+
+const userUsername = JSON.parse(
+    document.getElementById("user-username").textContent
+);
+
+const chatSocket = new WebSocket(
+    "ws://" +
+    window.location.host +
+    "/ws/chat/" +
+    roomName +
+    "/"
+);
+
+chatSocket.onmessage = function (event) {
+    const data = JSON.parse(event.data);
+    document.querySelector("#chat-text").value += (
+        data.user_name + ": " + data.message + "\n"
+    );
+}
+
+document.querySelector("#submit").onclick = function (event) {
+    const messageInputDom = document.querySelector("#input");
+    const message = messageInputDom.value;
+    chatSocket.send(
+        JSON.stringify(
+            {
+                "message": message,
+                "user_name": userUsername
+            }
+        )
+    );
+    messageInputDom.value = "";
+}
