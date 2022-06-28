2e344057fa29694321d11831ed092ebfa93115ac
SIIIKOR <siiikor.sikorski@gmail.com>
2022-06-28 21:11:01 +0200

    user rename

--- chat_app_project/add_data.py
+++ chat_app_project/add_data.py
@@ -2,7 +2,7 @@ from account.models import Account
 
 for i in range(10):
     Account.objects.create_user( # type: ignore
-        email=f"test{i}@gmail.com",
-        username=f"test{i}",
+        email=f"user{i}@gmail.com",
+        username=f"user{i}",
         password=f"1234"
     )
